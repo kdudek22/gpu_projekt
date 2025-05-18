c22ad0bf41028bfc9b39373ab57d7ee1179b0fbf
kdudek22 <appleofeden8686@gmail.com>
2025-05-18 21:41:31 +0200

    before hard refactor

--- main.py
+++ main.py
@@ -5,16 +5,13 @@ import numpy as np
 import open3d as o3d
 
 
-
-
 if __name__ == "__main__":
-
     camera_1 = Camera([-1000, 1, 1000], [-20, 160, 0], 60)
-    image_1 = cv2.imread("cam_1/image_0300.png", cv2.IMREAD_GRAYSCALE)
+    image_1 = cv2.imread("new_cam_1/image_0330.png", cv2.IMREAD_GRAYSCALE)
     line_set_1 = create_visualization_lines(image_1, camera_1)
 
     camera_2 = Camera([2000, 1, 0], [-25, -90, 0], 60)
-    image_2 = cv2.imread("cam_2/image_0500.png", cv2.IMREAD_GRAYSCALE)
+    image_2 = cv2.imread("new_cam_2/image_0330.png", cv2.IMREAD_GRAYSCALE)
     line_set_2 = create_visualization_lines(image_2, camera_2)
 
     min_corner = np.array([-1000, -1000, -1000])
