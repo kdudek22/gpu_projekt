adf5fb2eedd84d6a88762159559e8bf2f149780c
kdudek22 <appleofeden8686@gmail.com>
2025-06-01 10:46:42 +0200

    some more updates

--- actual_code/main.py
+++ actual_code/main.py
@@ -5,9 +5,12 @@ import numpy as np
 import cv2
 import matplotlib
 matplotlib.use('TkAgg')
+import matplotlib.cm as cm
+import matplotlib.colors as colors
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
 
 
 if __name__ == "__main__":
@@ -18,11 +21,11 @@ if __name__ == "__main__":
     mod = SourceModule(kernel_code)
     process_image = mod.get_function("process_image")
 
-    image_1 = cv2.imread("cam_1_30.png", cv2.IMREAD_GRAYSCALE)
-    image_2 = cv2.imread("cam_2_030.png", cv2.IMREAD_GRAYSCALE)
+    image_1 = cv2.imread("../recordings/4/frame_diffs/cam_1/image_150.png", cv2.IMREAD_GRAYSCALE)
+    image_2 = cv2.imread("../recordings/4/frame_diffs/cam_2/image_150.png", cv2.IMREAD_GRAYSCALE)
     # constant memory ?
-    camera_1_data = [1000, 1, 0, 0, -90, 0, 60]
-    camera_2_data = [0, 1, 0, 0, 90, 0, 60]
+    camera_1_data = [1000, 1, 150, 0, -60, 0, 60]
+    camera_2_data = [0, 1, 0, -10, 90, 0, 60]
 
     joined_camera_data = np.array(camera_1_data + camera_2_data).astype(np.int32)
 
@@ -40,12 +43,12 @@ if __name__ == "__main__":
     cuda.memcpy_htod(camera_data_gpu, joined_camera_data)
 
     # VOXEL SPACE
-    voxel_space_x_dim, voxel_space_y_dim, voxel_space_z_dim = 1000, 1000, 1000
+    voxel_space_x_dim, voxel_space_y_dim, voxel_space_z_dim = 25, 25, 25
 
     voxel_space = np.zeros(shape=(voxel_space_x_dim, voxel_space_y_dim, voxel_space_z_dim)).astype(np.int32)
     voxel_space_gpu = cuda.mem_alloc(voxel_space.nbytes)
     cuda.memcpy_htod(voxel_space_gpu, voxel_space)
-    voxel_space_unit = 1
+    voxel_space_unit = 40
 
     # KERNEL LAUNCH SETTINGS
     block_size = (16, 16, 1)  # block size, can update as long as TPD < 1024
@@ -59,14 +62,29 @@ if __name__ == "__main__":
     result = np.empty_like(voxel_space)
     cuda.memcpy_dtoh(result, voxel_space_gpu)
 
+    # Threshold to select voxels
+    mask = result > 1
+
+    # Normalize values for colormap
+    norm = Normalize(vmin=result[mask].min(), vmax=result[mask].max())
+    colormap = matplotlib.colormaps['viridis'] # Or cm.get_cmap('viridis')
+
+    # Create RGBA color array
+    colored_voxels = np.zeros((*result.shape, 4))  # (X, Y, Z, 4)
+    colored_voxels[mask] = colormap(norm(result[mask]))
+
+    # Plotting
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    ax.voxels(mask, facecolors=colored_voxels, edgecolor='k')
 
-    # Get coordinates of non-zero values
-    x, y, z = np.nonzero(result > 0.5)
+    ax.set_xlim([0, result.shape[0]])
+    ax.set_ylim([0, result.shape[1]])
+    ax.set_zlim([0, result.shape[2]])
 
-    ax.scatter(x, y, z)
-    ax.set_xlim([0, voxel_space_x_dim])
-    ax.set_ylim([0, voxel_space_y_dim])
-    ax.set_zlim([0, voxel_space_z_dim])
+    sm = cm.ScalarMappable(cmap=colormap, norm=norm)
+    sm.set_array([])  # Needed only to silence warnings
+    cbar = plt.colorbar(sm, ax=ax, pad=0.1)
+    cbar.set_label('Voxel Value')
+
     plt.show()
