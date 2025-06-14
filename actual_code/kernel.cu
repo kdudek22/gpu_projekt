7742a9b15db781b91dee8d30f70b2288ac9499f7
kdudek22 <appleofeden8686@gmail.com>
2025-06-14 17:28:13 +0200

    nicely working ball example

--- actual_code/kernel.cu
+++ actual_code/kernel.cu
@@ -54,10 +54,10 @@ extern "C" __global__ void process_image(int* img, int width, int height, int * 
     int cameraFov = cameraData[6 + image_index * 7];
 
     // ***** ACTUAL OPERATIONS START HERE *****
-    int pixelIndex = width * height * image_index + y * width + x;
+    int imageIndex = width * height * image_index + y * width + x;
 
     // If the value of the pixel is smaller than the threshold, do nothing :)
-    if(img[pixelIndex] < 10 || x >= width || y >= height){
+    if(img[imageIndex] < 100 || x >= width || y >= height){
         return;
     }
 
